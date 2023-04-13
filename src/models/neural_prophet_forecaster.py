c07eb60e78490a4cc4cd1fb3ff938951db55e3cc
Linus Folkerts <linus.folkerts@li-f.de>
2023-04-13 16:21:55 +0200

    Added model shell

---
+++ src/models/neural_prophet_forecaster.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+class NeuralProphetForecaster:
+    def __init__(self, historic_data: pd.DataFrame, forecasted_temperature: pd.DataFrame) -> None:
+        self.historic_data = historic_data
+        self.forecasted_temperature = forecasted_temperature
+
+    def __fit(self) -> None:
+        pass
+
+    def __prepare_prediction(self) -> None:
+        pass
+
+    def predict(self) -> pd.DataFrame:
+        return None
+    
+
+if __name__ == "__main__":    
+    pass
