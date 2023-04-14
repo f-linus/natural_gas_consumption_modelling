cb699ad6f69cbc9bbe357ac1032215bf5d24a55e
Linus Folkerts <linus.folkerts@li-f.de>
2023-04-14 16:35:59 +0200

    Added model fit method

--- src/models/temperature_forecaster.py
+++ src/models/temperature_forecaster.py
@@ -1,14 +1,38 @@
 import pandas as pd
 import pickle as pkl
 from src.data_handling import ingestion
+import neuralprophet as nprophet
 
 
 class TemperatureForecaster:
     def __init__() -> None:
         pass
 
-    def fit(self) -> None:
-        pass
+    def fit(self, model_file) -> None:
+
+        # Read temperature data
+        temperatures = ingestion.read_temperatures()
+        temperatures.index.name = "ds"
+
+        temperatures = temperatures.rename("y").to_frame().reset_index()
+
+        # Split data into train and test
+        train = temperatures[temperatures["ds"] < "2019-01-01"]
+        test = temperatures[temperatures["ds"] >= "2019-01-01"]
+
+        # Create model
+        model = nprophet.NeuralProphet(
+            n_forecasts=365,
+            n_lags=45,
+        )        
+
+        # Fit model
+        metrics = model.fit(train, freq="D")
+
+        # Save model
+        nprophet.save(model, model_file)
+
+        return metrics
 
     def predict(self) -> pd.DataFrame:
         return None
