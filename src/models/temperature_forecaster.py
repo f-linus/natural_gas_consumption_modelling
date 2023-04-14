4b294f93b3646834ba2dacf782568223b9b69203
Linus Folkerts <linus.folkerts@li-f.de>
2023-04-14 16:25:34 +0200

    Added temperature forecaster module

---
+++ src/models/temperature_forecaster.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import pickle as pkl
+from src.data_handling import ingestion
+
+
+class TemperatureForecaster:
+    def __init__() -> None:
+        pass
+
+    def fit(self) -> None:
+        pass
+
+    def predict(self) -> pd.DataFrame:
+        return None
+    
+    def load_model(self, model_file: str = "models/temperature_model.pkl") -> None:
+        pass
+
+if __name__ == "__main__":    
+    print("Hello")
