9169f1dac9dea6fa8ee36936a98467f81ad76ccc
Linus Folkerts <linus.folkerts@li-f.de>
2023-04-16 14:13:12 +0200

    Added forecasting method

--- src/models/consumption_forecaster.py
+++ src/models/consumption_forecaster.py
@@ -1,11 +1,15 @@
 import neuralprophet as nprophet
 import pandas as pd
 
+
 class ConsumptionForecaster:
     def __init__(self) -> None:
-        pass
+        self.train_df = None
+        self.model = None
 
-    def train(self, historic_consumption: pd.Series, historic_temperatures: pd.Series) -> None:
+    def train(
+        self, historic_consumption: pd.Series, historic_temperatures: pd.Series
+    ) -> None:
 
         # Cut consumption and temperature data to there index intersection
         historic_consumption = historic_consumption.loc[
@@ -20,7 +24,7 @@ class ConsumptionForecaster:
         temperatures_capped = historic_temperatures.apply(lambda x: min(18.0, x))
 
         # Create a dataframe with the consumption data and the temperature data
-        train_df = pd.DataFrame(
+        self.train_df = pd.DataFrame(
             {
                 "ds": historic_consumption.index,
                 "y": historic_consumption.values,
@@ -29,7 +33,9 @@ class ConsumptionForecaster:
         )
 
         # Add weekend column
-        train_df["weekend"] = (historic_consumption.index.weekday > 4).astype(float)
+        self.train_df["weekend"] = (historic_consumption.index.weekday > 4).astype(
+            float
+        )
 
         # Create model
         self.model = nprophet.NeuralProphet(n_forecasts=365, n_lags=15)
@@ -38,4 +44,30 @@ class ConsumptionForecaster:
         self.model.add_country_holidays(country_name="DE")
 
         # Fit model
-        return self.model.fit(train_df, epochs=400, progress="bar")
+        return self.model.fit(self.train_df, epochs=400, progress="bar")
+
+    def forecast(self, temperature_forecast: pd.Series) -> pd.Series:
+
+        # Cap temperature at 18.0 degrees
+        temperature_forecast = temperature_forecast.apply(lambda x: min(18.0, x))
+
+        future_regressors_df = temperature_forecast.rename("temperature_capped").to_frame()
+        future_regressors_df.index.name = "ds"
+        future_regressors_df.reset_index()
+
+        # Add weekend column to future regressors dataframe
+        future_regressors_df["weekend"] = (
+            temperature_forecast.index.weekday > 4
+        ).astype(float)
+
+        future = self.model.make_future_dataframe(
+            self.train_df,
+            periods=365,
+            regressors_df=future_regressors_df,
+        )
+        prediction = self.model.predict(future)
+
+        forecast = self.model.get_latest_forecast(prediction)
+        forecast = forecast.set_index("ds")["origin-0"].rename("y")
+
+        return forecast
