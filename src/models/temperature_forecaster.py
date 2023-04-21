0e4f61f370ce5dd51bf2454bb3d5a1dfb64cbcdf
Linus Folkerts <linus.folkerts@li-f.de>
2023-04-21 10:50:49 +0200

    Changed temperature moel

--- src/models/temperature_forecaster.py
+++ src/models/temperature_forecaster.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import pickle as pkl
 from src.data_handling import ingestion
 import neuralprophet as nprophet
 import requests
@@ -19,39 +18,6 @@ weather_data_cities = [
 class TemperatureForecaster:
     def __init__(self):
         self.model = None
-
-    def fit(self, model_file) -> None:
-        """Fit model and save it to disk.
-
-        Args:
-            model_file (str): Where to save the model.
-        """
-
-        # Read temperature data
-        temperatures = ingestion.read_temperatures()
-        temperatures.index.name = "ds"
-
-        temperatures = temperatures.rename("y").to_frame().reset_index()
-
-        # Split data into train and test
-        train = temperatures[temperatures["ds"] < "2019-01-01"]
-        test = temperatures[temperatures["ds"] >= "2019-01-01"]
-
-        # Create model
-        model = nprophet.NeuralProphet(
-            n_forecasts=365,
-            n_lags=45,
-        )
-
-        # Fit model
-        metrics = model.fit(train, freq="D")
-
-        # Save model
-        nprophet.save(model, model_file)
-
-        self.model = model
-
-        return metrics
 
     def load_model(self, model_file: str = "models/temperature_model.pkl") -> None:
         """Load model from disk.
@@ -143,7 +109,7 @@ if __name__ == "__main__":
     loaded = temperature_forecaster.load_model()
 
     if not loaded:
-        temperature_forecaster.fit()
+        raise Exception("Model not found!")
 
     forecast = temperature_forecaster.cobmined_forecast()
 
